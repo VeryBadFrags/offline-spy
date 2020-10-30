3824168a2acd6f085e808a3111b962e0d7bfb7bd
Loic Vourch <code@verybadfrags.com>
2020-10-29 18:13:32 -0700

    Simplify Python bundler

--- bundle.py
+++ bundle.py
@@ -1,6 +1,6 @@
+# Inputs
 source_folder = "src/"
 input_html = source_folder + "index.html"
-
 build_folder = "build/"
 input_css = build_folder + "style.css"
 constants_js = build_folder + "constants.js"
@@ -10,45 +10,34 @@ footer_html_path = build_folder + "footer.html"
 
 output_path = build_folder + "index.html"
 
+def inject_file_content(input_path, replacement_tag, destination_content, prefix="", suffix=""):
+    input_file = open(input_path, "r")
+    output_content = destination_content.replace(replacement_tag, prefix + input_file.read() + suffix)
+    input_file.close()
+    return output_content
+
 # Get HTML
 html_file = open(input_html, "r")
-content = html_file.read()
+output_html = html_file.read()
 html_file.close()
 
-# Inject Rules
-rules_file = open(rules_path, "r")
-content = content.replace("<!-- rules.html -->", rules_file.read())
-rules_file.close()
-
-# Inject Footer HTML
-footer_file = open(footer_html_path, "r")
-content = content.replace("<!-- footer.html -->", footer_file.read())
-footer_file.close()
+# Inject HTML Pages
+output_html = inject_file_content(rules_path, "<!-- rules.html -->", output_html)
+output_html = inject_file_content(footer_html_path, "<!-- footer.html -->", output_html)
 
 # Inject CSS
-css_file = open(input_css, "r")
-css_link = '<link rel="stylesheet" type="text/css" href="style.css">'
-content = content.replace(css_link, "<style>" + css_file.read() + "</style>")
-css_file.close()
+output_html = inject_file_content(input_css, '<link rel="stylesheet" type="text/css" href="style.css">', output_html, "<style>", "</style>")
 
 # Inject JS
 script_open = "<script>"
 script_close = "</script>"
 
-## Constants
-constants_file = open(constants_js, "r")
-content = content.replace('<script src="constants.js"></script>', script_open + constants_file.read() + script_close)
-constants_file.close()
-
-## Footer script
-footer_file = open(footer_js, "r")
-js_link = '<script src="footer.js"></script>'
-content = content.replace(js_link, script_open + footer_file.read() + script_close)
-footer_file.close()
+output_html = inject_file_content(constants_js, '<script src="constants.js"></script>', output_html, script_open, script_close)
+output_html = inject_file_content(footer_js, '<script src="footer.js"></script>', output_html, script_open, script_close)
 
 # Output content
 output_file = open(output_path, "w")
-output_file.write(content)
+output_file.write(output_html)
 output_file.close()
 
 print("Bundled " + input_html + " into " + output_path)
