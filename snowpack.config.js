9db05c895b83e2e6c6517f38537711aa1734bcc8
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2021-02-10 21:15:29 -0800

    Generate a static QR code
    Due to issues with running qrcode from make

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,22 +3,28 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-    mount: {
-        "src": { url: "/" },
-        "assets": { url: "/", static: true }
-    },
-    plugins: [
-        ['@snowpack/plugin-sass', {
-            compilerOptions: {
-                style: "compressed",
-                sourceMap: false,
-            }
-        }],
-        ['@snowpack/plugin-babel', {
-            transformOptions: {
-                presets: ['@babel/preset-env']
-            }
-        }],
-        ["@snowpack/plugin-optimize", { minifyCSS: false }],
+  mount: {
+    src: { url: "/" },
+    assets: { url: "/", static: true },
+  },
+  plugins: [
+    [
+      "@snowpack/plugin-sass",
+      {
+        compilerOptions: {
+          style: "compressed",
+          sourceMap: false,
+        },
+      },
     ],
+    [
+      "@snowpack/plugin-babel",
+      {
+        transformOptions: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+    ],
+    ["@snowpack/plugin-optimize", { minifyCSS: false }],
+  ],
 };
